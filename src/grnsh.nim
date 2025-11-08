a8aee3971ee38474a962c49530be89452992ed3b
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2025-11-09 01:16:41 +0300

    Create grnsh.nim

---
+++ src/grnsh.nim
@@ -0,0 +1,60 @@
+import asyncdispatch, httpclient, json
+
+const api = "https://api.grnsh.app/api/v1"
+var headers = newHttpHeaders({
+  "Connection": "keep-alive", 
+  "Host": "api.grnsh.app",
+  "Content-Type": "application/json", 
+  "accept": "application/json", 
+  "user-agent": "okhttp/4.12.0"
+})
+
+proc get_ingredients*(): Future[JsonNode] {.async.} =
+  let client = newAsyncHttpClient()
+  client.headers = headers
+  try:
+    let response = await client.get(api & "/ingredients")
+    let body = await response.body
+    result = parseJson(body)
+  finally:
+    client.close()
+
+proc get_ingredients_by_id*(id:int): Future[JsonNode] {.async.} =
+  let client = newAsyncHttpClient()
+  client.headers = headers
+  try:
+    let response = await client.get(api & "/ingredients/" & $id)
+    let body = await response.body
+    result = parseJson(body)
+  finally:
+    client.close()
+
+proc cocktails_with_ingredients*(page:int=1,order:string="pos",direction:string="ASC"): Future[JsonNode] {.async.} =
+  let client = newAsyncHttpClient()
+  client.headers = headers
+  try:
+    let response = await client.get(api & "/cocktails/withIngredients")
+    let body = await response.body
+    result = parseJson(body)
+  finally:
+    client.close()
+
+proc get_cocktails_by_id*(id:int=1): Future[JsonNode] {.async.} =
+  let client = newAsyncHttpClient()
+  client.headers = headers
+  try:
+    let response = await client.get(api & "/cocktails/" & $id)
+    let body = await response.body
+    result = parseJson(body)
+  finally:
+    client.close()
+
+proc get_cocktails*(page:int=1): Future[JsonNode] {.async.} =
+  let client = newAsyncHttpClient()
+  client.headers = headers
+  try:
+    let response = await client.get(api & "/cocktails")
+    let body = await response.body
+    result = parseJson(body)
+  finally:
+    client.close()
